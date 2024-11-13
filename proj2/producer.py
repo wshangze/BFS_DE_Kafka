3a61d468a96e3221ecec18677e0e56b5393f27c5
ruiallen <wangruihan36@gmail.com>
2024-11-13 11:41:54 -0500

    Modify comments

--- proj2/producer.py
+++ proj2/producer.py
@@ -1,3 +1,28 @@
+"""
+Copyright (C) 2024 BeaconFire Staffing Solutions
+Author: Ray Wang
+
+This file is part of Oct DE Batch Kafka Project 1 Assignment.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
 import csv
 import json
 import os
@@ -12,6 +37,8 @@ import psycopg2
 employee_topic_name = "bf_employee_cdc"
 
 class cdcProducer(Producer):
+    #if running outside Docker (i.e. producer is NOT in the docer-compose file): host = localhost and port = 29092
+    #if running inside Docker (i.e. producer IS IN the docer-compose file), host = 'kafka' or whatever name used for the kafka container, port = 9092
     def __init__(self, host="localhost", port="29092"):
         self.host = host
         self.port = port
